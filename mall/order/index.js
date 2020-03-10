b5066de060fc10cb75a1548ef785fd5adc38a3bd
qinsw <2573992956@qq.com>
2020-03-11 00:14:01 +0800

    优化主页图片排版，添加搜索页面内容

--- mall/order/index.js
+++ mall/order/index.js
@@ -1,4 +1,4 @@
-// mall/order/index.js
+import { request } from "../../request/index.js";
 Page({
 
   /**
@@ -24,13 +24,39 @@ Page({
     }],
   },
 
-  handleTabsItemChange(e) {
-    const { index } = e.detail;
+  onShow() {
+    // const token = wx.getStorageSync("token");
+    // if (!token) {
+    //   wx.navigateTo({
+    //     url: '/mall/auth/index',
+    //     complete: () => { }
+    //   });
+    //   return;
+    // }
+
+    let pages = getCurrentPages();
+    let currentPages = pages[pages.length - 1];
+    console.log(currentPages.options);
+    const { type } = currentPages.options;
+    this.changeItemType(type - 1);
+    this.getOrders(type);
+  },
+
+  async getOrders(type) {
+    const res = await request({ url: '/my/orders/all', data: { type } });
+    console.log(res);
+  },
+  changeItemType(index) {
     let { tabList } = this.data;
     tabList.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
     this.setData({
       tabList
     })
+  },
+  handleTabsItemChange(e) {
+    const { index } = e.detail;
+    this.changeItemType(index);
+    this.getOrders(index + 1);
   },
 
 
